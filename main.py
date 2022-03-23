d504f9fe06be8de69b37923843ed60f9b5ae4975
EJSRobinson <elliotjs.robinson@gmail.com>
2022-03-23 10:20:20 +0000

    Add S solvers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #import solvers.test as test
 import solvers.ct as ct
 import solvers.cr as cr
+import solvers.S as S
 from flask import Flask
 from flask_restful import Api
 
@@ -15,6 +16,9 @@ api.add_resource(ct.ct_3, '/ct/3')
 api.add_resource(cr.cr_1, '/cr/1')
 api.add_resource(cr.cr_2, '/cr/2')
 api.add_resource(cr.cr_3, '/cr/3')
+
+api.add_resource(S.S_1, '/S/1')
+api.add_resource(S.S_2, '/S/2')
 
 if __name__ == '__main__':
     app.run(debug=False)
