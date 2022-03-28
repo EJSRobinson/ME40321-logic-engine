484a31b0ede2000498a8aeeeec9244e6d3487ad0
EJSRobinson <elliotjs.robinson@gmail.com>
2022-03-28 21:19:55 +0100

    Correct Cna

--- solvers/Cna.py
+++ solvers/Cna.py
@@ -16,10 +16,11 @@ class Cna_1(Resource):
         CnaComp = response['CnaComp']
         Xcomp = response['Xcomp']
         Aref = response['Aref']
+
         Dref = (4*Aref/math.pi)**(1/2)  # Reference Length
         Xcp = Xcog + Kn*Dref    # Overall Cp
-        Cna1 = CnaComp * (Xcomp - Xcp) / (Xcp - Xfin)  # Single Fin
-        result = Cna1 * N
+        CnaTot = CnaComp * (Xcomp - Xcp) / (Xcp - Xfin)  # Single Fin
+        result = CnaTot / N
         return {'val': result}
 
 
@@ -31,14 +32,12 @@ class Cna_2(Resource):
         S = response['S']
         LEsw = response['LEsw']
         Aref = response['Aref']
-        N = response['N']
 
         Dref = (4*Aref/math.pi)**(1/2)
         Xr = S * math.tan(LEsw)
         Xlf = ct/2 + Xr - cr/2
         Lf = (Xlf**2 + S**2)**(1/2)
 
-        Cna1 = (1 + Dref/(S + Dref)) * (4*((S/Dref)**2)) / \
+        result = (1 + Dref/(S + Dref)) * (4*((S/Dref)**2)) / \
             (1 + (1 + (2 * Lf / (cr + ct))**2)**(1/2))
-        result = Cna1 * N
         return {'val': result}
