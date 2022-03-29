5af25180b12d8364a6678260a2a3b20450b13bc5
EJSRobinson <elliotjs.robinson@gmail.com>
2022-03-29 14:32:40 +0100

    Add further Afin solvers

--- solvers/Afin.py
+++ solvers/Afin.py
@@ -33,3 +33,54 @@ class Afin_3(Resource):
         m = response['m']
         result = m/(rowMat * t)
         return {'val': result}
+
+
+class Afin_4(Resource):
+    def post(self):
+        response = request.get_json()
+        Aref = response['Aref']
+        Cna = response['Cna']
+        TR = response['TR']
+        AR = response['AR']
+        TEsw = response['TEsw']
+        Dref = (4*Aref / math.pi)**(1/2)
+
+        while err > 0.000001:
+            cr = 2 * Afin**(1/2) / ((1+TR) * AR**(1/2))
+            LEsw = math.atan(cr*(1-TR)/((Afin * AR)**(1/2)) - math.tan(TEsw))
+            lfx = (cr/2) * (TR - 1) + (Afin * AR)**(1/2) * math.tan(LEsw)
+            lf = (lfx**2 + Afin*AR)**(1/2)
+            Block1 = (1 + (Dref/2)/((Afin * AR)**(1/2) + Dref/2))**(-1)
+            Block2 = ((4/Dref**2 * AR) /
+                      (1+(1+(2*lf/(cr*(1+TR)))**2)**(1/2)))**(-1)
+            AfinNew = Cna * Block1 * Block2
+            err = abs(Afin - AfinNew)
+            Afin = AfinNew
+
+        result = Afin
+        return {'val': result}
+
+
+class Afin_5(Resource):
+    def post(self):
+        response = request.get_json()
+        Aref = response['Aref']
+        Cna = response['Cna']
+        TR = response['TR']
+        AR = response['AR']
+        LEsw = response['LEsw']
+        Dref = (4*Aref / math.pi)**(1/2)
+
+        while err > 0.000001:
+            cr = 2 * Afin**(1/2) / ((1+TR) * AR**(1/2))
+            lfx = (cr/2) * (TR - 1) + (Afin * AR)**(1/2) * math.tan(LEsw)
+            lf = (lfx**2 + Afin*AR)**(1/2)
+            Block1 = (1 + (Dref/2)/((Afin * AR)**(1/2) + Dref/2))**(-1)
+            Block2 = ((4/Dref**2 * AR) /
+                      (1+(1+(2*lf/(cr*(1+TR)))**2)**(1/2)))**(-1)
+            AfinNew = Cna * Block1 * Block2
+            err = abs(Afin - AfinNew)
+            Afin = AfinNew
+
+        result = Afin
+        return {'val': result}
--- test.py
+++ test.py
@@ -1,10 +1,25 @@
 
 import math
-import solvers.common as common
 
-Alt = 10000
+Dref = 0.15
+Cna = 3.251
+TR = 0.5
+AR = 2
+TEsw = 0
+Afin = 0.05
 
+err = 100
 
-t = common.densityAtAltiude(Alt)
+while err > 0.000001:
+    cr = 2 * Afin**(1/2) / ((1+TR) * AR**(1/2))
+    LEsw = math.atan(cr*(1-TR)/((Afin * AR)**(1/2)) - math.tan(TEsw))
+    lfx = (cr/2) * (TR - 1) + (Afin * AR)**(1/2) * math.tan(LEsw)
+    lf = (lfx**2 + Afin*AR)**(1/2)
+    Block1 = (1 + (Dref/2)/((Afin * AR)**(1/2) + Dref/2))**(-1)
+    Block2 = ((4/Dref**2 * AR)/(1+(1+(2*lf/(cr*(1+TR)))**2)**(1/2)))**(-1)
+    AfinNew = Cna * Block1 * Block2
+    err = abs(Afin - AfinNew)
+    Afin = AfinNew
 
-print(t)
+print('Final Afin: '+str(Afin))
+print('Final Cr: '+str(cr))
