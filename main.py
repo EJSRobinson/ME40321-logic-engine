a9b7b352303689d15bdf1ca4a71b8a12ee82e64e
EJSRobinson <elliotjs.robinson@gmail.com>
2022-03-31 12:11:20 +0100

    Add Cbar solver

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ import solvers.Xfin as Xfin
 import solvers.CnaTot as CnaTot
 import solvers.CtaTot as CtaTot
 import solvers.RowA as RowA
+import solvers.Cbar as Cbar
 from flask import Flask
 from flask_restful import Api
 
@@ -108,6 +109,8 @@ api.add_resource(CnaTot.CnaTot_1, '/CnaTot/1')
 api.add_resource(CtaTot.CtaTot_1, '/CtaTot/1')
 
 api.add_resource(RowA.RowA_1, '/RowA/1')
+
+api.add_resource(Cbar.Cbar_1, '/Cbar/1')
 
 if __name__ == '__main__':
     app.run(debug=False)
---
+++ solvers/Cbar.py
@@ -0,0 +1,14 @@
+from flask_restful import reqparse, Resource
+from flask import request
+
+parser = reqparse.RequestParser()
+parser.add_argument('vars', type=str)
+
+
+class Cbar_1(Resource):
+    def post(self):
+        response = request.get_json()
+        cr = response['cr']
+        ct = response['t']
+        result = (2/3)*((cr ** 2 + ct ** 2 + cr*ct)/(cr + ct))
+        return {'val': result}
